b89050cfdc091040e6fffd1bd8956d794cfdfdb1
M.Srinivsas shankar <cherry@MSrinivsass-MacBook-Air.local>
2026-01-11 19:13:05 +0530

    Backend Server and db config setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './src/app.js';
+import {connectToDB} from './config/dbConfig.js';
+
+const PORT = process.env.PORT || 9000;
+
+connectToDB().then(()=>{
+    console.log('Starting server...');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((err)=>{
+    console.error('Failed to connect to the database', err);
+    process.exit(1);
+});
---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+//base config
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({extended: 'public'}));
+app.use(express.static('public'));
+
+app.use(
+    cors({
+        origin : process.env.CORS_ORIGIN?.split(','),
+        credentials: true,
+        methods : ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders : ['Content-Type', 'Authorization']
+    }));
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+export default app;
